8484a434363310600cd571ee456105bc77fbaea8
xBookbig <koonbookza2015@gmail.com>
2023-10-23 00:44:56 +0700

    Add ( File ) middleware

---
+++ middleware.tsx
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+  const isPublicPath = path === '/signin' || path === '/signup'
+  const token = request.cookies.get('secret-utcc')?.value || ''
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL('/', request.nextUrl))
+  }
+  
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL('/signin', request.nextUrl))
+  }
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/signin',
+    '/signup',
+  ]
+}
