743f14220084a9ed42b12859aa1c3c4ca40b7bd1
xBookbig <koonbookza2015@gmail.com>
2023-11-14 02:40:47 +0700

    Add ( function ) Etc.

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -1,6 +1,76 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 
-const Popup = ({ isOpen, buttonName }: any) => {
+const Popup = ({ isOpen, buttonName , course , fetchData }: any) => {
+  const [coursecode , setcoursecode] = useState('');
+  const [coursename , setcoursename] = useState('');
+  const [coursecredit, setcoursecredit] = useState(3);
+  const [convertcode , setconvertcode] = useState('');
+  const [convertname , setconvertname] = useState('');
+  const [category , setcategory] = useState('วิชาศึกษาทั่วไป');
+  const [convertcredit, setconvertcredit] = useState(3);
+  const [indexCourse , setindexCourse] = useState('');
+
+  useEffect(() => {
+    if (course) {
+      setindexCourse(course._id || '')
+      setcoursecode(course.CourseCode || '');
+      setcoursename(course.CourseName || '');
+      setcoursecredit(course.CourseCredit || 3);
+      setconvertcode(course.ConvertCode || '');
+      setconvertname(course.ConvertName || '');
+      setconvertcredit(course.ConvertCredit || 3);
+      setcategory(course.Category || 'วิชาศึกษาทั่วไป')
+    }
+  }, [course]);
+
+  const handlecoursecredit = (e: { target: { value: any; }; }) => {
+    setcoursecredit(e.target.value);
+  };
+
+  const handleconvertcredit = (e: { target: { value: any; }; }) => {
+    setconvertcredit(e.target.value);
+  };
+
+  const handlecategory = (e: { target: { value: any; }; }) => {
+    setcategory(e.target.value);
+  };
+
+  const handleSave = async () => {
+    const requestBody = {
+      indexCourse,
+      coursecode,
+      coursename,
+      coursecredit,
+      convertcode,
+      convertname,
+      convertcredit,
+      category,
+    };
+  
+    const fetchEndpoint = buttonName === 'add' ? 'addCompareSubject' : 'editCompareSubject';
+  
+    try {
+      const response = await fetch('http://localhost:8080/course/' + fetchEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      });
+  
+      if (!response.ok) {
+        const errorMessage = await response.text(); // Get the error message from the response body
+        throw new Error(`Failed to ${buttonName === 'add' ? 'add' : 'edit'} subject. Error: ${errorMessage}`);
+      }
+      isOpen(false)
+      fetchData()
+      console.log(`${buttonName === 'add' ? 'Add' : 'Edit'} request successful`);
+    } catch (error) {
+      console.error(`Error: ${error}`);
+    }
+  };
+  
   return (
     <div className="fixed top-1/4 left-1/2 -translate-x-1/2 bg-white w-1/3 z-50 rounded-sm shadow-lg p-4 border">
       <div>
@@ -15,24 +85,29 @@ const Popup = ({ isOpen, buttonName }: any) => {
         </p>
         <div>
           <div className="my-2">
-            <p>หมวดหมู่</p>
-            <select className="w-full rounded">
-              <option value="general">วิชาทั่วไป</option>
-              <option value="main">วิชาเอก</option>
-            </select>
-          </div>
-          <div className="my-2">
             <p>ชื่อวิชา</p>
-            <input type="text" className="w-full rounded" />
+            <input
+              required 
+              value={coursename}
+              onChange={(e) => setcoursename(e.target.value)}
+              type="text" 
+              className="w-full rounded" 
+            />
           </div>
           <div className="grid grid-cols-2 gap-5">
             <div>
               <p>รหัสวิชา</p>
-              <input type="text" className="w-full rounded" />
+              <input 
+                required
+                value={coursecode}
+                onChange={(e) => setcoursecode(e.target.value)}
+                type="text" 
+                className="w-full rounded" 
+              />
             </div>
             <div>
               <p>หน่วยกิต</p>
-              <select className="w-full rounded">
+              <select className="w-full rounded" value={coursecredit} onChange={handlecoursecredit}>
                 <option value={2}>2</option>
                 <option value={3}>3</option>
               </select>
@@ -46,23 +121,35 @@ const Popup = ({ isOpen, buttonName }: any) => {
         <div>
           <div className="my-2">
             <p>หมวดหมู่</p>
-            <select className="w-full rounded">
-              <option value="general">วิชาทั่วไป</option>
-              <option value="main">วิชาเอก</option>
+            <select className="w-full rounded" value={category} onChange={handlecategory}>
+              <option value="วิชาศึกษาทั่วไป">วิชาศึกษาทั่วไป</option>
+              <option value="วิชาเฉพาะ">วิชาเฉพาะ</option>
             </select>
           </div>
           <div className="my-2">
             <p>ชื่อวิชา</p>
-            <input type="text" className="w-full rounded" />
+            <input 
+              required
+              value={convertname}
+              onChange={(e) => setconvertname(e.target.value)}
+              type="text" 
+              className="w-full rounded" 
+            />
           </div>
           <div className="grid grid-cols-2 gap-5">
             <div>
               <p>รหัสวิชา</p>
-              <input type="text" className="w-full rounded" />
+              <input 
+                required
+                value={convertcode}
+                onChange={(e) => setconvertcode(e.target.value)}
+                type="text" 
+                className="w-full rounded" 
+              />
             </div>
             <div>
               <p>หน่วยกิต</p>
-              <select className="w-full rounded">
+              <select className="w-full rounded" value={convertcredit} onChange={handleconvertcredit}>
                 <option value={2}>2</option>
                 <option value={3}>3</option>
               </select>
@@ -71,7 +158,7 @@ const Popup = ({ isOpen, buttonName }: any) => {
         </div>
       </div>
       <div className="flex justify-end my-2">
-        <button className="p-2 text-sm font-bold text-white bg-blue-700 rounded-lg hover:bg-blue-800 m-2">
+        <button onClick={handleSave} className="p-2 text-sm font-bold text-white bg-blue-700 rounded-lg hover:bg-blue-800 m-2">
           SAVE
         </button>
         <button
