b7d5b41e70b3829d80a3ff629a09f3ac8a190483
xBookbig <koonbookza2015@gmail.com>
2023-11-14 02:41:08 +0700

    Add ( Function ) Etc.

--- components/CourseTable.tsx
+++ components/CourseTable.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -19,6 +19,7 @@ interface Column {
     | "ConvertCode"
     | "ConvertName"
     | "ConvertCredit"
+    | "Category"
     | "Setting";
   label: string;
   minWidth?: number;
@@ -27,12 +28,13 @@ interface Column {
 }
 
 const columns: readonly Column[] = [
-  { id: "CourseCode", label: "รหัสวิชา", minWidth: 170 },
+  { id: "CourseCode", label: "รหัสวิชา", minWidth: 100 },
   { id: "CourseName", label: "ชื่อวิชา", minWidth: 170 },
-  { id: "CourseCredit", label: "หน่วยกิต", minWidth: 170 },
-  { id: "ConvertCode", label: "รหัสวิชาเทียบโอน", minWidth: 170 },
+  { id: "CourseCredit", label: "หน่วยกิต", minWidth: 100 },
+  { id: "ConvertCode", label: "รหัสวิชาเทียบโอน", minWidth: 100 },
   { id: "ConvertName", label: "ชื่อวิชาเทียบโอน", minWidth: 170 },
-  { id: "ConvertCredit", label: "หน่วยกิตวิชาเทียบโอน", minWidth: 170 },
+  { id: "ConvertCredit", label: "หน่วยกิตวิชาเทียบโอน", minWidth: 100 },
+  { id: "Category", label: "หมวดหมู่", minWidth: 100 },
   { id: "Setting", label: "จัดการข้อมูล", minWidth: 170 },
 ];
 
@@ -43,6 +45,7 @@ interface Data {
   ConvertCode: string;
   ConvertName: string;
   ConvertCredit: number;
+  Category : string;
   _id: string;
   Setting?: string | number; // Add the 'setting' property with an optional string or number value
 }
@@ -54,6 +57,7 @@ function createData(
   ConvertCode: string,
   ConvertName: string,
   ConvertCredit: number,
+  Category : string,
   _id: string
 ): Data {
   return {
@@ -63,28 +67,60 @@ function createData(
     ConvertCode,
     ConvertName,
     ConvertCredit,
+    Category,
     _id,
   };
 }
-
-const rows = [
-  createData(
-    "30000-1201",
-    "ภาษาอังกฤษเพื่อการสื่อสาร",
-    3,
-    "GE071",
-    "ภาษาอังกฤษเพื่อการสื่อสารในชีวิตประจำวัน",
-    3,
-    "12312312312312"
-  ),
-];
 
 function CourseTable() {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
 
   const [isOpen, setIsOpen] = useState(false);
-  const [buttonName, setButtonName] = useState("");
+  const [buttonName, setButtonName] = useState('');
+
+  const [data, setData] = useState<Data[]>([]);
+
+  const [courseEdit , setcourseEdit] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/course', {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const result = await response.json();
+        const formattedData = result.courses.map((item: any) => {
+          return createData(
+            item.course_code,
+            item.course_name,
+            item.course_credit,
+            item.convert_code,
+            item.convert_name,
+            item.convert_credit,
+            item.category,
+            item._id
+          );
+        });
+        setData(formattedData);
+      } else {
+        console.error('Error fetching data');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleRefresh = () => {
+    fetchData();
+  };
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -97,9 +133,13 @@ function CourseTable() {
     setPage(0);
   };
 
-  const handleClickButton = (name: string) => {
+  const handleClickButton = (name: string, row?: any) => {
     setButtonName(name);
     setIsOpen(true);
+    setcourseEdit('');
+    if (name === 'edit' && row) {
+      setcourseEdit(row);
+    }
   };
 
   const SettingButton = ({ row }: { row: Data }) => {
@@ -108,7 +148,7 @@ function CourseTable() {
         <button
           type="button"
           className="px-4 py-2 text-sm font-bold text-white bg-green-500 border border-gray-200 rounded-l-lg hover:bg-green-800"
-          onClick={() => handleClickButton("edit")}
+          onClick={() => handleClickButton("edit", row)}
         >
           Edit
         </button>
@@ -124,7 +164,13 @@ function CourseTable() {
 
   return (
     <div className="relative max-w-[100rem] ml-auto mr-auto overflow-x-auto">
-      {isOpen && <Popup isOpen={setIsOpen} buttonName={buttonName} />}
+      {isOpen && 
+        <Popup 
+          isOpen={setIsOpen} 
+          buttonName={buttonName} 
+          course={courseEdit} 
+          fetchData={handleRefresh}
+        />}
 
       <div className="relative mt-8 mb-4 flex flex-row justify-between">
         <span className=" font-medium text-ms md:text-2xl">
@@ -155,14 +201,13 @@ function CourseTable() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows
+              {data
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row) => {
                   return (
                     <TableRow hover role="checkbox" tabIndex={-1} key={row._id}>
                       {columns.map((column) => {
                         const value = row[column.id];
-                        // let value = null;
                         return (
                           <TableCell key={column.id} align={column.align}>
                             {column.format && typeof value === "number"
@@ -183,7 +228,7 @@ function CourseTable() {
         <TablePagination
           rowsPerPageOptions={[10, 25, 100]}
           component="div"
-          count={rows.length}
+          count={data.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
